7232d9a9b627a0a99f4c6eb74a96db7c0031cb67
Joshua <153996013@qq.com>
2019-05-26 16:38:04 +0800

    all

--- src/applications/helper/udp-echo-helper.h
+++ src/applications/helper/udp-echo-helper.h
@@ -81,12 +81,13 @@ public:
    * Create one udp echo server application on each of the Nodes in the
    * NodeContainer.
    *
-   * \returns The applications created, one Application per Node in the 
+   * \returns The applications created, one Application per Node in the
    *          NodeContainer.
    */
   ApplicationContainer Install (NodeContainer c) const;
   void SetName(Ptr<Application> app, char* name);
   char* GetName(Ptr<Application> app);
+  void SetCloud(Ptr<Application> app);
 
 private:
   /**
@@ -135,7 +136,7 @@ public:
   void SetAttribute (std::string name, const AttributeValue &value);
 
   /**
-   * Given a pointer to a UdpEchoClient application, set the data fill of the 
+   * Given a pointer to a UdpEchoClient application, set the data fill of the
    * packet (what is sent as data to the server) to the contents of the fill
    * string (including the trailing zero terminator).
    *
@@ -149,12 +150,12 @@ public:
   void SetFill (Ptr<Application> app, std::string fill);
 
   /**
-   * Given a pointer to a UdpEchoClient application, set the data fill of the 
+   * Given a pointer to a UdpEchoClient application, set the data fill of the
    * packet (what is sent as data to the server) to the contents of the fill
    * byte.
    *
    * The fill byte will be used to initialize the contents of the data packet.
-   * 
+   *
    * \warning The size of resulting echo packets will be automatically adjusted
    * to reflect the dataLength parameter -- this means that the PacketSize
    * attribute may be changed as a result of this call.
@@ -166,12 +167,12 @@ public:
   void SetFill (Ptr<Application> app, uint8_t fill, uint32_t dataLength);
 
   /**
-   * Given a pointer to a UdpEchoClient application, set the data fill of the 
+   * Given a pointer to a UdpEchoClient application, set the data fill of the
    * packet (what is sent as data to the server) to the contents of the fill
    * buffer, repeated as many times as is required.
    *
    * Initializing the fill to the contents of a single buffer is accomplished
-   * by providing a complete buffer with fillLength set to your desired 
+   * by providing a complete buffer with fillLength set to your desired
    * dataLength
    *
    * \warning The size of resulting echo packets will be automatically adjusted
@@ -191,19 +192,19 @@ public:
    *
    * \param node The Ptr<Node> on which to create the UdpEchoClientApplication.
    *
-   * \returns An ApplicationContainer that holds a Ptr<Application> to the 
+   * \returns An ApplicationContainer that holds a Ptr<Application> to the
    *          application created
    */
   ApplicationContainer Install (Ptr<Node> node) const;
 
   /**
    * Create a udp echo client application on the specified node.  The Node
-   * is provided as a string name of a Node that has been previously 
+   * is provided as a string name of a Node that has been previously
    * associated using the Object Name Service.
    *
    * \param nodeName The name of the node on which to create the UdpEchoClientApplication
    *
-   * \returns An ApplicationContainer that holds a Ptr<Application> to the 
+   * \returns An ApplicationContainer that holds a Ptr<Application> to the
    *          application created
    */
   ApplicationContainer Install (std::string nodeName) const;
@@ -218,7 +219,7 @@ public:
   ApplicationContainer Install (NodeContainer c) const;
   void SetName(Ptr<Application> app, char* name);
   char* GetName(Ptr<Application> app);
-
+  void SetCloud(Ptr<Application> app);
 private:
   /**
    * Install an ns3::UdpEchoClient on the node configured with all the
@@ -234,4 +235,3 @@ private:
 } // namespace ns3
 
 #endif /* UDP_ECHO_HELPER_H */
-

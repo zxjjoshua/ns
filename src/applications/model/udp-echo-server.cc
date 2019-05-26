bcfdee70b3e90b6cb25e1284cb19f05a74e56a37
Joshua <153996013@qq.com>
2019-05-26 14:37:30 +0800

    all

--- src/applications/model/udp-echo-server.cc
+++ src/applications/model/udp-echo-server.cc
@@ -193,25 +193,25 @@ UdpEchoServer::HandleRead (Ptr<Socket> socket)
                        Inet6SocketAddress::ConvertFrom (from).GetIpv6 () << " port " <<
                        Inet6SocketAddress::ConvertFrom (from).GetPort ());
         }
+      //
+      // packet->RemoveAllPacketTags ();
+      // packet->RemoveAllByteTags ();
 
-      packet->RemoveAllPacketTags ();
-      packet->RemoveAllByteTags ();
-
-      NS_LOG_LOGIC ("Echoing packet");
-      socket->SendTo (packet, 0, from);
-
-      if (InetSocketAddress::IsMatchingType (from))
-        {
-          NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s server sent " << packet->GetSize () << " bytes to " <<
-                       InetSocketAddress::ConvertFrom (from).GetIpv4 () << " port " <<
-                       InetSocketAddress::ConvertFrom (from).GetPort ());
-        }
-      else if (Inet6SocketAddress::IsMatchingType (from))
-        {
-          NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s server sent " << packet->GetSize () << " bytes to " <<
-                       Inet6SocketAddress::ConvertFrom (from).GetIpv6 () << " port " <<
-                       Inet6SocketAddress::ConvertFrom (from).GetPort ());
-        }
+      // NS_LOG_LOGIC ("Echoing packet");
+      // socket->SendTo (packet, 0, from);
+      //
+      // if (InetSocketAddress::IsMatchingType (from))
+      //   {
+      //     NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s server sent " << packet->GetSize () << " bytes to " <<
+      //                  InetSocketAddress::ConvertFrom (from).GetIpv4 () << " port " <<
+      //                  InetSocketAddress::ConvertFrom (from).GetPort ());
+      //   }
+      // else if (Inet6SocketAddress::IsMatchingType (from))
+      //   {
+      //     NS_LOG_INFO ("At time " << Simulator::Now ().GetSeconds () << "s server sent " << packet->GetSize () << " bytes to " <<
+      //                  Inet6SocketAddress::ConvertFrom (from).GetIpv6 () << " port " <<
+      //                  Inet6SocketAddress::ConvertFrom (from).GetPort ());
+      //   }
     }
 }
 
